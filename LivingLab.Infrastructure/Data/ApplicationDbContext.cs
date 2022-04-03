6131c81bbe510262fb0eff22e73bc9467b65cc3d
Ng Han Yi <nghanyi1997@gmail.com>
2022-04-03 17:07:15 +0800

    remove CarbonFootprintEstimation and PowerGenerationMix

--- LivingLab.Infrastructure/Data/ApplicationDbContext.cs
+++ LivingLab.Infrastructure/Data/ApplicationDbContext.cs
@@ -16,8 +16,6 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<Device> Devices { get; set; }
     
     public DbSet<EnergyUsageLog> EnergyUsageLogs { get; set; }
-    public DbSet<PowerGenerationMix> PowerGenerationMix { get; set; }
-    public DbSet<CarbonFootprintEstimation> CarbonFootprintEstimations { get; set; }
     public DbSet<SmsLog> SmsLogs { get; set; }
     public DbSet<EmailLog> EmailLogs { get; set; }
     public DbSet<Accessory> Accessories { get; set; }
