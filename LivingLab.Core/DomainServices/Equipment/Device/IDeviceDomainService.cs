c51d37da0459649639a1767717a778d636c4a6e7
shengyu98 <shengyu98@hotmail.com>
2022-04-07 11:07:24 +0800

    documentation

--- LivingLab.Core/DomainServices/Equipment/Device/IDeviceDomainService.cs
+++ LivingLab.Core/DomainServices/Equipment/Device/IDeviceDomainService.cs
@@ -6,18 +6,59 @@ namespace LivingLab.Core.DomainServices.Equipment.Device;
 /// </remarks>
 public interface IDeviceDomainService
 {
+    /// <summary>
+    /// function to get a list of devices
+    /// </summary>
+    /// <returns></returns>
     Task<List<Entities.Device>> GetAllDevices();
+    
+    /// <summary>
+    /// function to get a list of devices based on device type and lab location
+    /// </summary>
+    /// <param name="deviceType"></param>
+    /// <param name="labLocation"></param>
+    /// <returns></returns>
     Task<List<Entities.Device>> ViewDevice(string deviceType, string labLocation);
     
-    /***
-    * For P1-5 you can use this method below to populate the device list in the lab profile,
-    * by injecting this IDeviceDomainService in your UI's lab service.
-    */
+    /// <summary>
+    /// function to get a list of available devices to populate in the lablocation
+    /// </summary>
+    /// <param name="labLocation"></param>
+    /// <returns></returns>
     Task<List<Entities.Device>> GetDevicesForLabProfile(string labLocation);
+    
+    /// <summary>
+    /// function to update the review status
+    /// </summary>
+    /// <param name="deviceId"></param>
+    /// <param name="deviceReviewStatus"></param>
     void UpdateDeviceStatus(string deviceId, string deviceReviewStatus);
+    
+    /// <summary>
+    /// function to get all the devices to review, based on lab location
+    /// </summary>
+    /// <param name="labLocation"></param>
+    /// <returns></returns>
     Task<List<Entities.Device>> GetAllDevicesForReview(string labLocation);
+    
+    /// <summary>
+    /// function to get a list of devices and its quantity based on each device type
+    /// </summary>
+    /// <param name="labLocation"></param>
+    /// <returns></returns>
     Task<List<ViewDeviceTypeDTO>> ViewDeviceType(string labLocation);
+    
+    /// <summary>
+    /// function to populate the edit device pop up modal 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     Task<Entities.Device> ViewDeviceDetails(int id);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
     Task<Entities.Device> GetDeviceLastRow();
     Task<Entities.Device> AddDevice(Entities.Device addedDevice);
     Task<Entities.Device> EditDeviceDetails(Entities.Device editedDevice);
