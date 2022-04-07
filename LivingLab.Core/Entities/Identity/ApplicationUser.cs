73d04dbef46925306695954370c7dd943196e32d
bellesim <bellesimbeier@gmail.com>
2022-04-07 15:46:28 +0800

    added author for viewmodel/csthml/entity

--- LivingLab.Core/Entities/Identity/ApplicationUser.cs
+++ LivingLab.Core/Entities/Identity/ApplicationUser.cs
@@ -5,16 +5,12 @@ using LivingLab.Core.Enums;
 using Microsoft.AspNetCore.Identity;
 
 namespace LivingLab.Core.Entities.Identity;
-
+/// <remarks>
+/// Author: Team P1-5
+/// </remarks>
 public class ApplicationUser : IdentityUser
-{
-    // Add additional profile data for application users by adding properties to this class
-    public string? FirstName { get; set; }
+{ public string? FirstName { get; set; }
     public string? LastName { get; set; }
-    // NotificationEmails and NotificationSMS references are required
-    // to force Entity Framework to recognize the many-to-many relationship
-    // but based on our design, applicationuser should not hold reference to notification messages
-    // so they have been made private
     private List<EmailLog> NotificationEmails { get; set; } = new List<EmailLog>();
     private List<SmsLog> NotificationSmses { get; set; } = new List<SmsLog>();
     public NotificationType PreferredNotification { get; set; }
